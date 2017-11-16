5ff7f1d9ae394754e35e7eefcf7c5f4864734fe8
Jason Decker <jasondecker@Portatile-Flatiron.local>
2017-11-16 16:29:37 -0500

    Add messages route

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,18 @@
 class Api::V1::MessagesController < ApplicationController
   def create
-    @message = Message.create(alias:params[:alias],message:params[:message])
-    render json:@message
+    message = Message.create(alias:params[:alias],message:params[:message])
+    render json:message
+  end
+
+  def update
+    message = Message.find(params[:id])
+    message.update(alias:params[:alias],message:params[:message])
+    message.save
+    render json:message
+  end
+
+  def index
+    message = Message.order(created_at:'desc')
+    render json:message
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post '/messages', to:'messages#create'
+      patch '/messages/:id', to:'messages#update'
+      get '/messages', to:'messages#index'
     end
   end
 end
